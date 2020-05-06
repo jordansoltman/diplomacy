50a5a71bc781771c36386d8f2d9bcd939bb97a6b
Tyler <tylersayvetz@gmail.com>
2020-05-06 16:28:37 -0700

    scaffold home view w/ dummy data, add icons

---
+++ frontend/src/types.ts
@@ -0,0 +1,34 @@
+/**
+ * The status of a given players' orders for a given turn.
+ */
+export enum GameTurnState {
+    NO_ORDERS,
+    SAVED,
+    COMMITTED
+}
+
+/**
+ * Player in a given game
+ */
+export interface GamePlayer {
+    name: string;
+    state: GameTurnState;
+    //more data for the players here.
+}
+
+/**
+ * The game data that the front end knows about.
+ */
+export interface GameListData {
+    id: string;
+    name: string;
+    numPlayers: number;
+    size: number;
+    turnNumber: number;
+    winCondition: number;
+    messages: number;
+    unread: boolean;
+    nextTurn: number;
+    players: GamePlayer[];
+    ownStatus: GameTurnState;
+}
